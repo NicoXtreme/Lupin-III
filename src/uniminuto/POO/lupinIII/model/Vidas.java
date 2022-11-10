723e1d82d4013ff54c37c9d41ea9174a4c4500ff
JanethOliveros <117890868+JanethOliveros@users.noreply.github.com>
2022-11-10 01:13:14 -0500

    Create Vidas.java

---
+++ src/uniminuto/POO/lupinIII/model/Vidas.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uniminuto.POO.lupinIII.model;
+
+
+/**
+ *
+ * @author Ojitos
+ */
+public class Vidas {
+    /* la variable que va a controlar las vidas
+    */
+   int vidas;
+   
+   public Vidas(){
+       vidas = 3;
+    }
+   
+   public int getVidas(){
+      return vidas; 
+   }
+   
+   public void setVidas(int vidas){
+      this.vidas = vidas; 
+   }
+   
+}
